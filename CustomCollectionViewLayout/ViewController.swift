50a385275983ed4843c3e57f4ce91ce378a1d4ef
donnywals <donnywals@gmail.com>
2015-07-07 20:35:45 +0200

    Initial Commit

---
+++ CustomCollectionViewLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomCollectionViewLayout
+//
+//  Created by Donny Wals on 07-07-15.
+//  Copyright (c) 2015 Donny Wals. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
